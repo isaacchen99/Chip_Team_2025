01fd3593c63eb9a67e2b92d8ba694d8965d5a42c
toby-zheng <tobyjzheng@gmail.com>
2025-04-13 14:12:28 -0500

    convolution rewrite, python hex generation fix

--- CNN/ieee_cnn/verilog/sliding_window.sv
+++ CNN/ieee_cnn/verilog/sliding_window.sv
@@ -1,58 +1,48 @@
 module sliding_window #(
     parameter WORD_SIZE = 8, 
-    parameter BUFFER_SIZE = 3, 
+    parameter KERNEL_DIM = 3, 
     parameter ROW_SIZE = 540 
 )
 (
     input logic clk, rst,
     input logic [WORD_SIZE-1:0] inputPixel,
-    output logic [WORD_SIZE-1:0] window[BUFFER_SIZE-1:0][BUFFER_SIZE-1:0]
+    output logic [WORD_SIZE-1:0] window[KERNEL_DIM-1:0][KERNEL_DIM-1:0]
 );
 
-    logic [WORD_SIZE-1:0] buffer[ROW_SIZE-1:0][BUFFER_SIZE-1:0];
+    logic [WORD_SIZE-1:0] buffer[ROW_SIZE-1:0][KERNEL_DIM-1:0];
     logic [$clog2(ROW_SIZE)-1:0] ptr;
 
     always_ff @(posedge clk) begin
         if (rst) begin
             ptr <= 0;
-            for (int i = 0; i < BUFFER_SIZE; i++) begin
-                for (int j = 0; j < BUFFER_SIZE; j++) begin
-                    window[i][j] <= 0;
+            for (int i = 0; i < KERNEL_DIM; i++) begin
+                for (int j = 0; j < KERNEL_DIM; j++) begin
+                window[i][j] <= 0;
                 end
             end
         end 
         else begin
             // shift downward
-            for (int i = BUFFER_SIZE-1; i > 0; i--) begin
-                for (int j = 0; j < BUFFER_SIZE; j++) begin
-                    $display("window[%0d][%0d] = %h", i, j, window[i][j]);
-                    window[i][j] <= window[i-1][j];
-                end
-            end
+            window[2][0] <= window[1][0];
+            window[2][1] <= window[1][1];
+            window[2][2] <= window[1][2];
+            window[1][0] <= window[0][0];
+            window[1][1] <= window[0][1];
+            window[1][2] <= window[0][2];
 
-            // restore from buffer
-            if (ptr > BUFFER_SIZE-1) begin
-                for (int j = 0; j < BUFFER_SIZE; j++) begin
-                    window[0][j] <= buffer[ptr-1][j];
-                end
-            end
-            else begin
-                // shift right
-                for (int j = BUFFER_SIZE-1; j > 0; j--) begin
-                    window[0][j] <= window[0][j-1];
-                end
-                window[0][0] <= inputPixel;
-            end
+            // shift first row right
+            window[0][0] <= inputPixel;
+            window[0][1] <= window[0][0]; 
+            window[0][2] <= window[0][1]; 
 
-            // store last row to buffer
-            for (int j = 0; j < BUFFER_SIZE; j++) begin
-                buffer[ptr][j] <= window[BUFFER_SIZE-1][j];
-            end
+            // buffer data
+            buffer[ptr] <= window[KERNEL_DIM-1];
+            window[0] <= buffer[ptr];
 
-            // update pointer
-            if (ptr < ROW_SIZE - BUFFER_SIZE) begin
+            // increment ptr
+            if(ptr < ROW_SIZE - KERNEL_DIM) begin
                 ptr <= ptr + 1;
-            end 
+            end
             else begin
                 ptr <= 0;
             end
